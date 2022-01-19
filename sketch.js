78857276ccd20411bf5244f22d824897a964e1b4
Manasvi Narayanan <mnaray5@uic.edu>
2022-01-19 10:36:02 -0600

    adding unfilled circles

--- sketch.js
+++ sketch.js
@@ -4,11 +4,23 @@ function setup() {
   
   function draw() {
     background("#B1E4C4");
+
     fill("#C0B1E4");
     noStroke();
     circle(50,60,80);
+
+    noFill();
+    stroke("#9486B5");
+    strokeWeight(1);
+    circle(60,70,80);
+
     fill("#B1CBE4");
     noStroke();
     circle(200,150,50);
+
+    noFill();
+    stroke("#76899B");
+    strokeWeight(1);
+    circle(190,140,50);
 
   }
