d921491bb7a2331876d10c537d34a387e6df4498
Manasvi Narayanan <mnaray5@uic.edu>
2022-01-19 10:25:40 -0600

    adding circles

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(500, 500);
   }
   
   function draw() {
-    background(120);
+    background("#B1E4C4");
+    fill("#C0B1E4");
+    noStroke();
+    circle(50,60,80);
+    fill("#B1CBE4");
+    noStroke();
+    circle(200,150,50);
+
   }
